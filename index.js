7744d103db4c8eae54d21db32143a554e7de961a
kareemali2001 <147373986+kareemali2001@users.noreply.github.com>
2024-05-19 13:00:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+import qr from "qr-image" ;
+import fs from "fs";
+inquirer
+  .prompt([{
+   message:"type your url:",
+    name:"URL",
+  },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr-img.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+      
+    }
+  });
+
+ 
+
+ 
